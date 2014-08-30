7f46ae2e95d1cab10284f2eb11ba80590adc2627
yanxin <359063370@qq.com>
2014-08-30 13:00:43 +0800

    add Classes and Resource

---
+++ Classes/BulletLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  BulletLayer.cpp
+//  tranckFighting
+//
+//  Created by Yan on 14-8-29.
+//
+//
+
+#include "BulletLayer.h"
---
+++ Classes/GameScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameScene.cpp
+//  tranckFighting
+//
+//  Created by Yan on 14-8-29.
+//
+//
+
+#include "GameScene.h"
