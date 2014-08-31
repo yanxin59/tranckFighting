e930c876cd2a7edf0ab3c2dcda5a2f7f3dc76a2d
yanxin <359063370@qq.com>
2014-08-31 20:12:34 +0800

    Update GameScene.cpp
    yanxin merge

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -7,32 +7,21 @@
 //
 
 #include "GameScene.h"
-<<<<<<< HEAD
 #include "ControllerLayer.h"
-bool GameScene::init(){
-    
-    if (!Scene::init()) {
-        return false;
-    }
-    
-    auto layer = ControllerLayer::create();
-    
-    this->addChild(layer);
-    
-    
-    
-    
-    return true;
-=======
 #include "EnemyLayer.h"
 #include "BulletLayer.h"
+
 
 bool GameScene::init(){
 	if (!Scene::init()){
 		return false;
 	}
+	auto layer = ControllerLayer::create();
+    	this->addChild(layer);
+    	
 	this->addChild(EnemyLayer::create());
 	auto l = BulletLayer::create();
+	
 	l->setTag(1);
 	this->addChild(l);
 	return true;
@@ -40,5 +29,4 @@ bool GameScene::init(){
 
 BulletLayer* GameScene::getLayer(){
 	return dynamic_cast<BulletLayer*>(this->getChildByTag(1));
->>>>>>> c4bbd9cd6e5e193394b45530f9f4da713d3f46c9
 }
