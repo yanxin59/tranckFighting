a548ad4c957efc8cec5508ee6c71393169d7b15c
yanxin <359063370@qq.com>
2014-08-31 22:44:10 +0800

    normal version

---
+++ Classes/stopScene.h
@@ -0,0 +1,33 @@
+//
+//  stopScene.h
+//  tranckFighting
+//
+//  Created by 王紫叶 on 14-8-30.
+//
+//
+
+#ifndef __tranckFighting__stopScene__
+#define __tranckFighting__stopScene__
+
+#include <iostream>
+#include "cocos2d.h"
+using namespace cocos2d;
+class stopScene:public Scene{
+    
+protected:
+    
+    virtual bool init();
+    
+public:
+    
+    CREATE_FUNC(stopScene);
+    
+    
+};
+
+
+
+
+
+
+#endif /* defined(__tranckFighting__stopScene__) */
