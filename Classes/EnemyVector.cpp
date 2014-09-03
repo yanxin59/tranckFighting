275f4ac180e8dd25f1bfcc5ff7d112bcfe01988e
wangzhen <wzdzh4321@163.com>
2014-09-03 17:57:16 +0800

    wangzhen

--- Classes/EnemyVector.cpp
+++ Classes/EnemyVector.cpp
@@ -22,3 +22,15 @@ void EnemyVector::reset(){
 void EnemyVector::clearEnemy(){
 	enemyVector.clear();
 }
+void EnemyVector::delayTime(){
+	for(int i = 0;i < 4;i++){
+		Enemy * enemy = enemyVector.at(i);
+		enemy->unscheduleAllSelectors();
+		DelayTime * dt = DelayTime::create(10);
+		CallFunc * cf = CallFunc::create([=](){
+			enemy->resumeSchedulerAndActions();
+		});
+		Sequence * seq = Sequence::create(dt,cf,NULL);
+		enemy->runAction(seq);
+	}
+}
